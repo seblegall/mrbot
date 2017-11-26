410c97108bf1d53a01ec6b1f0953291ee3edf84b
Sébastien Le Gall <seb@le-gall.net>
2017-11-26 22:49:48 +0100

    Add configuration management using viper

--- mrbot.go
+++ mrbot.go
@@ -1,31 +1,61 @@
 package main
 
 import (
-	"github.com/seblegall/mrbot/pkg/hipchat"
-)
+	"fmt"
 
-const (
-	// HipChat jabber info
-	hipChatJabberURL  = "chat.hipchat.com"
-	hipChatJabberPort = 5223
+	"github.com/seblegall/mrbot/pkg/hipchat"
+	"github.com/spf13/viper"
 )
 
 var (
-	resource = "bot" // Kind of Hipchat user (probably shouldn't change this)
+	//Hipchat
+	hipChatJabberURL  string
+	hipChatJabberPort int
+	username          string
+	mentionname       string
+	fullname          string
+	password          string
+	roomJid           string
 
-	// Vars needed for Hipbot to ping Hipchat:
-	username    = "175921_5350262"
-	mentionname = "mrbot"
-	fullname    = "Mr Bot"
-	password    = "xcem7sa2"
-	roomJid     = "175921_testbot@conf.hipchat.com"
+	//Gitlab
+	token     string
+	gitlabURL string
+	groups    []string
 )
 
 func main() {
+	setConfig()
 
 	hipchat := hipchat.NewClient(hipChatJabberURL, hipChatJabberPort, username, password)
 	room := hipchat.NewRoom(roomJid, fullname)
 	bot := NewBot(hipchat, room)
 	bot.Join()
 	bot.ListenAndAnswer()
+	bot.ListenMergeRequest()
+}
+
+func setConfig() {
+	viper.SetConfigName(".mrbot")
+	viper.AddConfigPath("$HOME/")
+	viper.AddConfigPath(".")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+
+	//Hipchat configuration
+	hipChatJabberURL = viper.GetString("hipchat.server")
+	hipChatJabberPort = viper.GetInt("hipchat.port")
+	username = viper.GetString("hipchat.username")
+	mentionname = viper.GetString("hipchat.mentionname")
+	fullname = viper.GetString("hipchat.fullname")
+	password = viper.GetString("hipchat.password")
+	roomJid = viper.GetString("hipchat.roomJid")
+
+	//Gitlab configuration
+	token = viper.GetString("gitlab.token")
+	gitlabURL = viper.GetString("gitlab.url")
+	groups = viper.GetStringSlice("gitlab.groups")
+
 }
